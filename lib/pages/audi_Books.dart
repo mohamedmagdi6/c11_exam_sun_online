6841ca2f1ce5ea8dca0658253da0b0733d8fd537
mohamedmagdi6 <mohamedmagdii137@gmail.com>
2024-07-31 16:37:26 +0300

    audiBooks had finished

---
+++ lib/pages/audi_Books.dart
@@ -0,0 +1,365 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+class AudiBooks extends StatelessWidget {
+  AudiBooks({super.key});
+
+  List<String> imageList = [
+    'assets/images/4f10107bed2dbba9192d48cb63c24565.jpg',
+    'assets/images/65e13454961c9be7df9bad116fd82b48.png',
+    'assets/images/2e40cba2f80b9f15c88722fc12d3ab47.png',
+    'assets/images/25d5a95e7c4978c40c7ed763ebf1274f.jpg',
+    'assets/images/1580a11820a456b161bc330d3b78472a.jpg',
+    'assets/images/608609cb613363a4bec0992fd87c1f9f.jpg',
+    'assets/images/e348831994111b27cd102cbcbbe5230d.jpg',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        titleSpacing: 8,
+        leadingWidth: 80,
+        toolbarHeight: 100,
+        leading: Padding(
+          padding: const EdgeInsets.only(top: 46, left: 28),
+          child: Image.asset('assets/icons/Logo Small.png'),
+        ),
+        title: Padding(
+          padding: const EdgeInsets.only(
+            top: 46,
+          ),
+          child: Row(
+            children: [
+              Text(
+                'Audi',
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  color: Color(0xFF4838D1),
+                  fontSize: 24,
+                ),
+              ),
+              Text(
+                'Books',
+                style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                  color: Color(0xFF4838D1),
+                  fontSize: 24,
+                ),
+              ),
+              Text(
+                '.',
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  color: Color(0xFFe8805e),
+                  fontSize: 24,
+                ),
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(top: 46, right: 28),
+            child: ImageIcon(
+              AssetImage('assets/icons/Setting.png'),
+              size: 50,
+              color: Color(0xFF4838D1),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Color(0xFF4838D1),
+          selectedLabelStyle: TextStyle(
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.w500,
+            fontSize: 10,
+          ),
+          type: BottomNavigationBarType.fixed,
+          items: const [
+            BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage('assets/icons/Home.png')),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage('assets/icons/Search.png')),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage('assets/icons/Document.png')),
+              label: 'Library',
+            ),
+          ]),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 24, right: 24, top: 30),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Categories',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'See more',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xFF4838D1),
+                        ),
+                      ))
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                height: 40,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    Container(
+                      width: 55,
+                      padding: EdgeInsets.symmetric(horizontal: 8),
+                      child: Center(
+                        child: Text(
+                          'Art',
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w400,
+                              fontSize: 16,
+                              color: Color(0xFF2E2E5D)),
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF5F5FA),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 12,
+                    ),
+                    Container(
+                      width: 102,
+                      padding: EdgeInsets.symmetric(horizontal: 8),
+                      child: Center(
+                        child: Text(
+                          'Business',
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w400,
+                              fontSize: 16,
+                              color: Color(0xFF2E2E5D)),
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF5F5FA),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 12,
+                    ),
+                    Container(
+                      width: 101,
+                      padding: EdgeInsets.symmetric(horizontal: 8),
+                      child: Center(
+                        child: Text(
+                          'Comedy',
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w400,
+                              fontSize: 16,
+                              color: Color(0xFF2E2E5D)),
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF5F5FA),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 12,
+                    ),
+                    Container(
+                      width: 102,
+                      padding: EdgeInsets.symmetric(horizontal: 8),
+                      child: Center(
+                        child: Text(
+                          'Drama',
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w400,
+                              fontSize: 16,
+                              color: Color(0xFF2E2E5D)),
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF5F5FA),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Recommended For You',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'See more',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xFF4838D1),
+                        ),
+                      ))
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              CarouselSlider.builder(
+                options: CarouselOptions(
+                  height: 300,
+                  viewportFraction: 0.58,
+                  enableInfiniteScroll: false,
+                ),
+                itemCount: imageList.length,
+                itemBuilder:
+                    (BuildContext context, int itemIndex, int pageViewIndex) =>
+                        Container(
+                  width: 200,
+                  child: Image.asset(
+                    imageList[itemIndex],
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Best Sellet',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'See more',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xFF4838D1),
+                        ),
+                      ))
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                height: 144,
+                child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        width: 315,
+                        decoration: BoxDecoration(
+                          color: Color(0xFFF5F5FA),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(12),
+                          child: Row(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(4),
+                                child: Image.asset(
+                                  imageList[index],
+                                  width: 120,
+                                  height: 120,
+                                  fit: BoxFit.fill,
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(left: 20, top: 10),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'COLD',
+                                      style: TextStyle(
+                                        fontFamily: 'Poppins',
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 5,
+                                    ),
+                                    Text(
+                                      'JEFF CARSON',
+                                      style: TextStyle(
+                                          fontFamily: 'Poppins',
+                                          fontWeight: FontWeight.w400,
+                                          fontSize: 12,
+                                          color: Color(0xFF6A6A8B)),
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return SizedBox(
+                        width: 16,
+                      );
+                    },
+                    itemCount: imageList.length),
+              ),
+              SizedBox(
+                height: 30,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
